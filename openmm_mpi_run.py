6078c2b4c2f4883b3e9e2863b6649283b895c57b
Austin Clyde <aclyde@uchicago.edu>
2019-10-26 21:29:42 -0500

    added openmm_mpi_run code.

---
+++ openmm_mpi_run.py
@@ -0,0 +1,22 @@
+'''
+author: Austin Clyde
+date: 10/26/2019
+
+sources:
+'''
+
+from charm4py import charm
+from impress_md import interface_functions
+
+def run_mmgbsa(path):
+    interface_functions.RunMinimization(path, path, True)
+
+def main(args):
+    #file listing of paths to run this on, pass as argument. One line per path.
+    with open('file_list.txt', 'r') as f:
+        paths_to_run_mmgbsa = map(lambda x : x.strip(), f.readlines())
+
+    result = charm.pool.map(run_mmgbsa, paths_to_run_mmgbsa, chunksize=6)
+    exit()
+
+charm.start(main)
